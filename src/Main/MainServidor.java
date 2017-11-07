5205273161a94fbfa7abda6e544e0797ca74b613
Gabriel Zawalski <gabrielzawalski@gmail.com>
2017-11-06 23:56:04 -0200

    inicio interface servidor

---
+++ src/Main/MainServidor.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import ViewServidor.GUIServidor;
+/**
+ *
+ * @author gabriel
+ */
+public class MainServidor {
+    public static void main(String[] args) {
+        new GUIServidor().setVisible(true);
+    }
+}
